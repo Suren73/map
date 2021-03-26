3208c9355aff290b60b5d16d897d9a927c9b9b3d
Andrei Surkov <andronsurkov73@gmail.com>
2021-03-26 16:25:52 +0300

    поправил код

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -1,6 +1,15 @@
 import ErrorRepository from '../app';
 
 test('testing class ErrorRepository', () => {
+  const expected = new Map([
+    [0, 'Ошибка входа'],
+    [1, 'Ошибка добавления'],
+  ]);
+  const received = new ErrorRepository().errorMap;
+  expect(received).toEqual(expected);
+});
+
+test('testing class ErrorRepository', () => {
   const expected = 'Ошибка входа';
   const received = new ErrorRepository().translate(0);
   expect(received).toBe(expected);
